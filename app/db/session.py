407623b96d8d802d3244ffa95395c60881faa5fe
hammi837 <hammadhafeez526@gmail.com>
2026-07-10 00:56:39 +0500

    Fix import statement for app.models in session.py to adhere to code style guidelines

--- app/db/session.py
+++ app/db/session.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session, sessionmaker
 from app.core.config import settings
 from app.db.base import Base
 
+import app.models  # noqa: F401
+
 engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
